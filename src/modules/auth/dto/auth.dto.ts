c048167b2d325bafa545bbb3a6348253272eb72f
Altesse Royal <altessemwamboko@gmail.com>
2026-07-05 18:13:13 +0100

    Create dto/auth.dto.ts

---
+++ src/modules/auth/dto/auth.dto.ts
@@ -0,0 +1,27 @@
+import { IsEmail, IsString, MinLength, IsOptional } from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(3)
+  username: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsOptional()
+  @IsString()
+  mobileNumber?: string;
+}
+
+export class LoginDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
