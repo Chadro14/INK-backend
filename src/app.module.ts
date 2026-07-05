63ed2dae0a240894a5c0e9288a07e3fe8534913c
Altesse Royal <altessemwamboko@gmail.com>
2026-07-05 18:07:53 +0100

    Create app.module.ts

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { MangasModule } from './modules/mangas/mangas.module';
+import { SocialModule } from './modules/social/social.module';
+import { PaymentsModule } from './modules/payments/payments.module';
+import { InkstreamModule } from './modules/inkstream/inkstream.module';
+import { AdminModule } from './modules/admin/admin.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    MangasModule,
+    SocialModule,
+    PaymentsModule,
+    InkstreamModule,
+    AdminModule,
+    DashboardModule,
+  ],
+})
+export class AppModule {}
