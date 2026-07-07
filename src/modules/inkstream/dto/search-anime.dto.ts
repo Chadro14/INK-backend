d6b9a829575d934906b9019e61b076ef8372bbc2
Altesse Royal <altessemwamboko@gmail.com>
2026-07-07 02:12:38 +0100

    Create search-anime.dto.ts

---
+++ src/modules/inkstream/dto/search-anime.dto.ts
@@ -0,0 +1,29 @@
+import { IsOptional, IsString, IsInt, Min, Max } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class SearchAnimeDto {
+  @IsOptional()
+  @IsString()
+  q?: string;
+
+  @IsOptional()
+  @IsString()
+  genre?: string;
+
+  @IsOptional()
+  @IsString()
+  source?: string;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page?: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(50)
+  limit?: number = 20;
+}
