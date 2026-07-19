34427986eaab4cea89d980a706f50c85682989bb
Altesse Royal <altessemwamboko@gmail.com>
2026-07-19 22:04:20 +0100

    Create user-filter.dto.ts

---
+++ src/modules/admin/dto/user-filter.dto.ts
@@ -0,0 +1,33 @@
+import { IsOptional, IsString, IsBoolean, IsInt, Min, Max } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class UserFilterDto {
+  @IsOptional()
+  @IsString()
+  search?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isCertified?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  isVerified?: boolean;
+
+  @IsOptional()
+  @IsString()
+  role?: string;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page?: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(50)
+  limit?: number = 20;
+}
