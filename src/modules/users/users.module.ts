1048db49ce96d643fcd8e279b6f5a3bcbfdfc7e5
Altesse Royal <altessemwamboko@gmail.com>
2026-07-24 03:06:59 +0200

    Update users.module.ts

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { StorageService } from '../../common/services/storage.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, StorageService],
   exports: [UsersService],
 })
 export class UsersModule {}
