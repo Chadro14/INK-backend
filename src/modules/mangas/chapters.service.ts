dd1baf8fcd0d6de599e9267f6b173e75943b2fdc
Altesse Royal <altessemwamboko@gmail.com>
2026-07-26 01:51:19 +0200

    Update chapters.service.ts

--- src/modules/mangas/chapters.service.ts
+++ src/modules/mangas/chapters.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import { CreateChapterDto } from './dto/create-chapter.dto';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
@@ -8,10 +8,10 @@ export class ChaptersService {
   private supabase: SupabaseClient;
 
   constructor(private readonly prisma: PrismaService) {
-    // Initialisation de Supabase avec tes variables d'environnement
+    // Initialisation de Supabase avec tes variables d'environnement Vercel
     this.supabase = createClient(
       process.env.SUPABASE_URL,
-      process.env.SUPABASE_KEY
+      process.env.SUPABASE_SERVICE_ROLE_KEY
     );
   }
 
@@ -43,7 +43,7 @@ export class ChaptersService {
     let pdfSize = null;
 
     // ==========================================
-    // UPLOAD DES IMAGES (BUCKET 1 : CHAPTERS1)
+    // UPLOAD DES IMAGES (BUCKET : CHAPTERS1)
     // ==========================================
     if (contentType === 'IMAGES' && imageFiles) {
       pageCount = imageFiles.length;
@@ -76,14 +76,13 @@ export class ChaptersService {
       }
     } 
     // ==========================================
-    // UPLOAD DU PDF (BUCKET 2)
+    // UPLOAD DU PDF (BUCKET : CHAPTERS1)
     // ==========================================
     else if (contentType === 'PDF' && pdfFile) {
       pageCount = 1; 
       pdfSize = pdfFile.size;
       const fileName = `${mangaId}/chapitre-${dto.number}/${Date.now()}_${pdfFile.originalname}`;
 
-      // ⚠️ REMPLACE 'NOM_DU_BUCKET_PDF' PAR LE VRAI NOM DE TON 2EME BUCKET
       const { error } = await this.supabase.storage
         .from('CHAPTERS1') 
         .upload(fileName, pdfFile.buffer, {
@@ -96,7 +95,7 @@ export class ChaptersService {
 
       // Récupération du lien public
       const { data: publicUrlData } = this.supabase.storage
-        .from('NOM_DU_BUCKET_PDF')
+        .from('CHAPTERS1')
         .getPublicUrl(fileName);
 
       pdfKey = publicUrlData.publicUrl;
