d7b6c954a2c03aade85150a87259ac800bc48069
Altesse Royal <altessemwamboko@gmail.com>
2026-08-08 23:23:06 +0200

    Update mangas.module.ts

--- src/modules/mangas/mangas.module.ts
+++ src/modules/mangas/mangas.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MangasController } from './mangas.controller';
 import { MangasService } from './mangas.service';
+import { ChaptersController } from './chapters.controller'; // ✅ AJOUTER
 import { ChaptersService } from './chapters.service';
 import { StorageService } from '../../common/services/storage.service';
-import { PrismaModule } from '../../prisma/prisma.module'; // ✅ IMPORTANT
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule], // ✅ POURQUE PRISMA SOIT DISPONIBLE
-  controllers: [MangasController],
+  imports: [PrismaModule],
+  controllers: [
+    MangasController,
+    ChaptersController, // ✅ AJOUTER CETTE LIGNE
+  ],
   providers: [MangasService, ChaptersService, StorageService],
   exports: [MangasService, ChaptersService],
 })
