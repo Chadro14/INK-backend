08548e5868b983b61b668be15578d61c242c2d6c
Altesse Royal <altessemwamboko@gmail.com>
2026-08-11 00:08:25 +0200

    Update manga-api.module.ts

--- src/modules/manga-api/manga-api.module.ts
+++ src/modules/manga-api/manga-api.module.ts
@@ -1,15 +1,8 @@
 import { Module } from '@nestjs/common';
-import { HttpModule } from '@nestjs/axios';
 import { MangaApiService } from './manga-api.service';
 import { MangaApiController } from './manga-api.controller';
 
 @Module({
-  imports: [
-    HttpModule.register({
-      timeout: 30000,
-      maxRedirects: 5,
-    }),
-  ],
   controllers: [MangaApiController],
   providers: [MangaApiService],
   exports: [MangaApiService],
