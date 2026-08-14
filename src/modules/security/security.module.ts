5d9574937f6accd12b94330e21931a8494a0c8b0
Altesse Royal <altessemwamboko@gmail.com>
2026-08-14 08:47:30 +0200

    Create security.module.ts

---
+++ src/modules/security/security.module.ts
@@ -0,0 +1,13 @@
+// src/modules/security/security.module.ts
+import { Module } from '@nestjs/common';
+import { SecurityService } from './security.service';
+import { SecurityController } from './security.controller';
+import { PrismaModule } from '../../prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [SecurityController],
+  providers: [SecurityService],
+  exports: [SecurityService],
+})
+export class SecurityModule {}
