db2b5dcd6b9bca2e8a7cced9bfda84337c81703a
Altesse Royal <altessemwamboko@gmail.com>
2026-08-14 09:44:10 +0200

    Create common.module.ts

---
+++ src/common/common.module.ts
@@ -0,0 +1,10 @@
+// src/common/common.module.ts
+import { Module, Global } from '@nestjs/common';
+import { EmailService } from './services/email.service';
+
+@Global()
+@Module({
+  providers: [EmailService],
+  exports: [EmailService],
+})
+export class CommonModule {}
