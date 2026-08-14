b0caf066745988346c5ec6b1b2ef126679dd868d
Altesse Royal <altessemwamboko@gmail.com>
2026-08-14 22:03:08 +0200

    Update users.module.ts

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { StorageService } from '../../common/services/storage.service';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, StorageService],
+  providers: [UsersService, StorageService, PrismaService],
   exports: [UsersService],
 })
 export class UsersModule {}
