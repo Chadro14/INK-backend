3ed67c091dc1d175fff6573947c184f7d589fdab
Altesse Royal <altessemwamboko@gmail.com>
2026-08-14 21:19:59 +0200

    Update security.module.ts

--- src/modules/security/security.module.ts
+++ src/modules/security/security.module.ts
@@ -3,9 +3,13 @@ import { Module } from '@nestjs/common';
 import { SecurityService } from './security.service';
 import { SecurityController } from './security.controller';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { CommonModule } from '../../common/common.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    CommonModule,
+  ],
   controllers: [SecurityController],
   providers: [SecurityService],
   exports: [SecurityService],
