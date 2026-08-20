d86ce324cdf5db2be2b3245cf7ab194d1184ebe1
Altesse Royal <altessemwamboko@gmail.com>
2026-08-20 21:53:20 +0200

    Create premium.module.ts

---
+++ src/modules/premium/premium.module.ts
@@ -0,0 +1,13 @@
+// src/modules/premium/premium.module.ts
+import { Module } from '@nestjs/common';
+import { PremiumController } from './premium.controller';
+import { PremiumService } from './premium.service';
+import { PrismaModule } from '../../prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [PremiumController],
+  providers: [PremiumService],
+  exports: [PremiumService],
+})
+export class PremiumModule {}
