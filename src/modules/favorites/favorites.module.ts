82f6402eb89616b57a608eaa74ff90d4422da691
Altesse Royal <altessemwamboko@gmail.com>
2026-08-23 17:26:24 +0200

    Create favorites.module.ts

---
+++ src/modules/favorites/favorites.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { FavoritesController } from './favorites.controller';
+import { FavoritesService } from './favorites.service';
+
+@Module({
+  controllers: [FavoritesController],
+  providers: [FavoritesService, PrismaService],
+  exports: [FavoritesService],
+})
+export class FavoritesModule {}
