328c6906cf7ffb61293bf9d7cbc61ebbec4cb6d2
Altesse Royal <altessemwamboko@gmail.com>
2026-08-25 21:57:45 +0200

    Delete src/modules/notifications/index.ts

--- src/modules/notifications/index.ts
+++
@@ -1,5 +0,0 @@
-// src/modules/notifications/index.ts
-export { NotificationsModule } from './notifications.module';
-export { NotificationsService } from './notifications.service';
-export { NotificationsGateway } from './notifications.gateway';
-export { NotificationsController } from './notifications.controller';
