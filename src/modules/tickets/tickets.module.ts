41414430890c27795bba63f2399542425ffeb220
Altesse Royal <altessemwamboko@gmail.com>
2026-08-28 05:37:46 +0200

    Create tickets.module.ts

---
+++ src/modules/tickets/tickets.module.ts
@@ -0,0 +1,13 @@
+// src/modules/tickets/tickets.module.ts
+import { Module } from '@nestjs/common';
+import { TicketsController } from './tickets.controller';
+import { TicketsService } from './tickets.service';
+import { PrismaModule } from '../../prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [TicketsController],
+  providers: [TicketsService],
+  exports: [TicketsService],
+})
+export class TicketsModule {}
