501f5f7ab0abf7f25d610f2351d2446f43c26bb3
Altesse Royal <altessemwamboko@gmail.com>
2026-08-31 18:27:09 +0200

    Update events.module.ts

--- src/modules/events/events.module.ts
+++ src/modules/events/events.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
-import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
+import { EventsService } from './events.service';
+import { EventSchedulerService } from './event-scheduler.service';
+import { EventVotingService } from './event-voting.service';
+import { EventRewardsService } from './event-rewards.service';
+import { EventRankingService } from './event-ranking.service';
+import { PrismaModule } from '../../prisma/prisma.module';
+import { ManasModule } from '../manas/manas.module';
+import { TicketsModule } from '../tickets/tickets.module';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { AuditModule } from '../audit/audit.module';
 
 @Module({
+  imports: [
+    PrismaModule,
+    ManasModule,
+    TicketsModule,
+    NotificationsModule,
+    AuditModule,
+  ],
   controllers: [EventsController],
-  providers: [EventsService],
-  exports: [EventsService],
+  providers: [
+    EventsService,
+    EventSchedulerService,
+    EventVotingService,
+    EventRewardsService,
+    EventRankingService,
+  ],
+  exports: [
+    EventsService,
+    EventSchedulerService,
+    EventVotingService,
+    EventRewardsService,
+    EventRankingService,
+  ],
 })
 export class EventsModule {}
