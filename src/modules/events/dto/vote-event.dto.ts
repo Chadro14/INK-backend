03559fdf80ce6d7581fca1e307b7f0f18164b688
Altesse Royal <altessemwamboko@gmail.com>
2026-09-02 13:36:11 +0200

    Update vote-event.dto.ts

--- src/modules/events/dto/vote-event.dto.ts
+++ src/modules/events/dto/vote-event.dto.ts
@@ -1,14 +1,5 @@
 import { IsEnum, IsUUID, IsNotEmpty } from 'class-validator';
-
-export enum VoteType {
-  UP = 'UP',
-  DOWN = 'DOWN',
-  STAR_1 = 'STAR_1',
-  STAR_2 = 'STAR_2',
-  STAR_3 = 'STAR_3',
-  STAR_4 = 'STAR_4',
-  STAR_5 = 'STAR_5',
-}
+import { VoteType } from '@prisma/client'; // ✅ Utiliser l'enum de Prisma directement
 
 export class VoteEventDto {
   @IsUUID()
