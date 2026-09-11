fb21fd9d5584d80ca77f4db06fee034a1227c070
Altesse Royal <altessemwamboko@gmail.com>
2026-09-11 20:25:57 +0200

    Add ReelSchedulerService to ReelsModule providers

--- src/modules/reels/reels.module.ts
+++ src/modules/reels/reels.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ReelsController } from './reels.controller';
 import { ReelsService } from './reels.service';
+import { ReelSchedulerService } from './reel-scheduler.service';
 import { PrismaService } from '../../prisma/prisma.service';
 import { StorageService } from '../../common/services/storage.service';
 
 @Module({
   controllers: [ReelsController],
-  providers: [ReelsService, PrismaService, StorageService],
+  providers: [
+    ReelsService,
+    ReelSchedulerService, // ✅ AJOUT
+    PrismaService,
+    StorageService,
+  ],
   exports: [ReelsService],
 })
 export class ReelsModule {}
