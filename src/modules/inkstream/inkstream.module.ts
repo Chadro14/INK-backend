99c19ce4cb8778ffda5c109009c8d58c55d31dfc
Altesse Royal <altessemwamboko@gmail.com>
2026-09-12 21:51:35 +0200

    Update inkstream.module.ts

--- src/modules/inkstream/inkstream.module.ts
+++ src/modules/inkstream/inkstream.module.ts
@@ -1,12 +1,12 @@
-// src/modules/inkstream/inkstream.module.ts
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { InkstreamController } from './controllers/inkstream.controller';
 import { InkstreamService } from './services/inkstream.service';
 import { MovieboxService } from './services/moviebox.service';
 import { ScraperService } from './services/scraper.service';
-import { ManasService } from '../manas/manas.service'; // ✅ CHANGER VERS ../manas/manas.service
+import { ManasService } from '../manas/manas.service';
 import { PrismaModule } from '../../prisma/prisma.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
   imports: [
@@ -15,13 +15,14 @@ import { PrismaModule } from '../../prisma/prisma.module';
       maxRedirects: 5,
     }),
     PrismaModule,
+    NotificationsModule,
   ],
   controllers: [InkstreamController],
   providers: [
     InkstreamService,
     MovieboxService,
     ScraperService,
-    ManasService, // ✅ UTILISE CELUI DE ../manas
+    ManasService,
   ],
   exports: [InkstreamService],
 })
