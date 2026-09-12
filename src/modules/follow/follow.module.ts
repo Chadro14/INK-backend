cc98cd06cc92a9a152247f1afd04ec269d69c72b
Altesse Royal <altessemwamboko@gmail.com>
2026-09-12 15:50:00 +0200

    Update follow.module.ts

--- src/modules/follow/follow.module.ts
+++ src/modules/follow/follow.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FollowService } from './follow.service';
 import { FollowController } from './follow.controller';
+import { PrismaModule } from '../../prisma/prisma.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
+  imports: [PrismaModule, NotificationsModule],
   controllers: [FollowController],
   providers: [FollowService],
   exports: [FollowService],
