19f4ba86d45a805f3c09bb85d1db3cf2b4b565a4
Altesse Royal <altessemwamboko@gmail.com>
2026-09-13 19:03:00 +0200

    Update manas.module.ts

--- src/modules/manas/manas.module.ts
+++ src/modules/manas/manas.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ManasService } from './manas.service';
 import { ManasController } from './manas.controller';
+import { BalanceService } from './balance.service';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
   imports: [PrismaModule, NotificationsModule],
   controllers: [ManasController],
-  providers: [ManasService],
-  exports: [ManasService],
+  providers: [ManasService, BalanceService],
+  exports: [ManasService, BalanceService],
 })
 export class ManasModule {}
